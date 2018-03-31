b460cc867d7f765823f4ea1f4a37aa0ac2407ab1
Benny <carlsson.benny93@gmail.com>
2018-03-31 12:35:02 +0200

    sign in with Google,Facebook,Twitter,Github,Email

--- src/App.js
+++ src/App.js
@@ -1,44 +1,72 @@
 import React from "react"
-import "./App.css"
 import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import { Jumbotron, Button, Image } from "react-bootstrap"
+import "./App.css"
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD_PKvd7ag-k-N-Rvu3KbSsLqfLG_92PaY",
+  authDomain: "react-firebase-auth-ace44.firebaseapp.com"
+})
 
 class App extends React.Component {
-  componentDidMount = () => {
-    var app = firebase.initializeApp({
-      apiKey: "AIzaSyD_PKvd7ag-k-N-Rvu3KbSsLqfLG_92PaY",
-      authDomain: "react-firebase-auth-ace44.firebaseapp.com"
-    })
-    console.log("app", app)
+  state = {
+    isSignedIn: false
   }
-  login = () => {
-    var provider = new firebase.auth.GoogleAuthProvider()
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken
-        // The signed-in user info.
-        var user = result.user
-        // ...
-        console.log("token", token)
-        console.log("user", result.user)
-      })
-      .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code
-        var errorMessage = error.message
-        // The email of the user's account used.
-        var email = error.email
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential
-        // ...
-      })
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+    })
+    var user = firebase.auth().currentUser
+
+    if (user) {
+      this.setState({ isSignedIn: true })
+    } else {
+      this.setState({ isSignedIn: false })
+    }
   }
   render() {
     return (
       <div className="App">
-        <button onClick={() => this.login()}>Login</button>
+        {this.state.isSignedIn ? (
+          <span>
+            <Jumbotron>
+              <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+              <Button onClick={() => firebase.auth().signOut()}>
+                Sign out
+              </Button>
+            </Jumbotron>
+
+            <Image
+              alt="profile"
+              src={firebase.auth().currentUser.photoURL}
+              circle
+            />
+          </span>
+        ) : (
+          <span>
+            <Jumbotron>
+              <h1>Sign In</h1>
+            </Jumbotron>
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          </span>
+        )}
       </div>
     )
   }
